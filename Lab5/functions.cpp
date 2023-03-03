a05f46511db06604c225536d5d9877f0176b99fb
Dhruv Patel <114122837+dhruvpatel-98@users.noreply.github.com>
2023-03-02 20:58:31 -0500

    Add files via upload

--- Lab5/functions.cpp
+++ Lab5/functions.cpp
@@ -6,15 +6,15 @@
 #include "Lab5.h"
 using namespace std;
 
-vector <double_vector> read_file(string file1) {
-    vector <double_vector> vector_collection;
+vector <double_vect> read_file(string file1) {
+    vector <double_vect> vector_collection;
     ifstream input_file(file1);
     if (input_file.is_open()) {
         int id_temp = 0;
         double from_temp;
         double to_temp;
         while(input_file >> from_temp >> to_temp) {
-            double_vector temp_vector;
+            double_vect temp_vector;
             temp_vector.id = id_temp;
             temp_vector.from = from_temp;
             temp_vector.to = to_temp;
@@ -25,21 +25,21 @@ vector <double_vector> read_file(string file1) {
     return vector_collection;
 }
 
-double dot_prod(double_vector v1, double_vector v2) {
+double dot_prod(double_vect v1, double_vect v2) {
     return (v1.from * v2.from) + (v1.to * v2.to);
 }
 
-double vector_len(double_vector v1) {
+double vector_len(double_vect v1) {
     return sqrt(dot_prod(v1, v1));
 }
 
-double cosine_dist(double_vector v1, double_vector v2) {
+double cosine_dist(double_vect v1, double_vect v2) {
     double dot = dot_prod(v1, v2);
     double chicago = vector_len(v1) * vector_len(v2);
     return dot / chicago;
 }
 
-vector <pairs> calculate_cosine_pairs(vector <double_vector> my_vector){
+vector <pairs> calculate_cosine_pairs(vector <double_vect> my_vector){
     vector <pairs> cosine_pairs;
     for (int i = 0; i < my_vector.size()-1; i++) {
         for (int j = i + 1; j < my_vector.size(); j++) {
